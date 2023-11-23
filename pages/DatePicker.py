78143503e43a3e543531958d686f730a332c5d8c
observedobserver <270001151@qq.com>
2023-11-22 16:15:10 -0800

    feat: date picker support label

--- pages/DatePicker.py
+++ pages/DatePicker.py
@@ -2,6 +2,6 @@ import streamlit as st
 import streamlit_shadcn_ui as ui
 
 st.header("Date Picker")
-dt = ui.date_picker(key="date_picker")
+dt = ui.date_picker(key="date_picker", label="Date Picker")
 
 st.write("Date:", dt)
--- streamlit_shadcn_ui/py_components/date_picker.py
+++ streamlit_shadcn_ui/py_components/date_picker.py
@@ -5,15 +5,17 @@ from streamlit_extras.stylable_container import stylable_container
 from streamlit_shadcn_ui.py_components.utils.session import init_session 
 import streamlit as st
 
-def date_picker_trigger(value=None, open_status=False, key=None):
+_RELEASE = False
+
+def date_picker_trigger(value = None, label: str = None, open_status = False, key = None):
     name = "date_picker_trigger"
-    _component_func = declare_component(name)
-    props = {"value": value, "open": open_status}
+    _component_func = declare_component(name, release=_RELEASE)
+    props = {"value": value, "open": open_status, "label": label}
     return _component_func(comp=name, props=props, key=key,  default={"x": 0, "y": 0, "open": False})
 
 def date_picker_content(x: int, y: int, value=None, default_value=None, open: bool = False, key=None, on_change=None, args=None, kwargs=None):
     name = "date_picker_content"
-    _component_func = declare_component(name)
+    _component_func = declare_component(name, release=_RELEASE)
     register_callback(key=key, callback=on_change, args=args, kwargs=kwargs)
     container = stylable_container(key="cont", css_styles=f"""
         {{
@@ -39,8 +41,6 @@ def date_picker(label=None, default_value=None, key=None):
     init_session(key=trigger_component_key, default_value={"x": 0, "y": 0, "open": False})
     init_session(key=content_component_key, default_value={"value": default_value, "open": False})
     open_status = st.session_state[trigger_component_key]['open']
-    st.write(st.session_state[trigger_component_key], open_status)
-    st.write(st.session_state[content_component_key], open_status)
     with stylable_container(key="all", css_styles="""
                             
             {
@@ -48,7 +48,7 @@ def date_picker(label=None, default_value=None, key=None):
             }
             """):
         value = st.session_state[content_component_key]['value']
-        trigger_pos = date_picker_trigger(value=value, open_status=open_status, key=trigger_component_key)
+        trigger_pos = date_picker_trigger(value=value, label=label, open_status=open_status, key=trigger_component_key)
         # need to sync value, or "cancel" will not work
         st.session_state[content_component_key]['open'] = trigger_pos['open']
         content_state = date_picker_content(value=value, x=trigger_pos['x'], y=trigger_pos['y'], open=open_status, key=content_component_key, on_change=date_choosen_handler, kwargs={"from_key": content_component_key, "to_key": trigger_component_key})
