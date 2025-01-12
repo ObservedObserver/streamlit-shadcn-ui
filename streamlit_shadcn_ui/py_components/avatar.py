a620f4df4c552a6760a1c0416754b34904888000
Jingting Feng <feng@Jingtings-MacBook-Pro.local>
2025-01-12 11:27:51 -0800

    Removed unnecessary comments

--- streamlit_shadcn_ui/py_components/avatar.py
+++ streamlit_shadcn_ui/py_components/avatar.py
@@ -4,7 +4,5 @@ _component_func = declare_component("avatar")
 
 def avatar(src: str, fallback: str = None, key = None):
     props = {"src": src, "fallback": fallback}
-    # fallback是默认返回值，可以没有
-    # component_value存返回值，没有的话返回None
-    component_value = _component_func(comp="avatar", props=props, key=key, default=None)# 我要在前端渲染一个组件
+    component_value = _component_func(comp="avatar", props=props, key=key, default=None)
     return component_value
