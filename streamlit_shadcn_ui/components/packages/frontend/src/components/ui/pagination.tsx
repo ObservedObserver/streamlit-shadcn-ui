3dbe80e710c89f61731e09408aae36223fde8dcc
observedobserver <270001151@qq.com>
2025-02-09 11:03:33 +0800

    fix: pagination & global shadcn config

--- streamlit_shadcn_ui/components/packages/frontend/src/components/ui/pagination.tsx
+++ streamlit_shadcn_ui/components/packages/frontend/src/components/ui/pagination.tsx
@@ -4,14 +4,19 @@ import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react"
 import { cn } from "@/lib/utils"
 import { ButtonProps, buttonVariants } from "@/components/ui/button"
 
-const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
+const Pagination = React.forwardRef<
+  HTMLDivElement,
+  React.ComponentProps<"nav">
+>(({ className, ...props }, ref) => (
   <nav
+    ref={ref}
     role="navigation"
     aria-label="pagination"
     className={cn("mx-auto flex w-full justify-center", className)}
     {...props}
   />
-)
+))
+
 Pagination.displayName = "Pagination"
 
 const PaginationContent = React.forwardRef<
